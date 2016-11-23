f9d4263ad74f382e328b40e1aea6f2234b452427
rian adam <rian.adam.r@gmail.com>
2016-11-23 18:00:02 +0700

    use hmset instead set

--- run.py
+++ run.py
@@ -8,18 +8,25 @@ app = Flask(__name__)
 app.config.update(dict(SECRET_KEY='yoursecretkey'))
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+# set auto-generated key
+if(r.exists('id')==False):
+    r.set('id','0')
+
 class CreateTask(Form):
     title = TextField('Task Title')
+    shortdesc = TextField('Short Description')
     priority = IntegerField('Priority')
     create = SubmitField('Create')
 
 class DeleteTask(Form):
     key = TextField('Task Key')
+    title = TextField('Task Title')
     delete = SubmitField('Delete')
 
 class UpdateTask(Form):
     key = TextField('Task Key')
     title = TextField('Task Title')
+    shortdesc = TextField('Short Description')
     priority = IntegerField('Priority')
     update = SubmitField('Update')
 
@@ -29,28 +36,41 @@ class ResetTask(Form):
 def createTask(form):
     title = form.title.data
     priority = form.priority.data
+    shortdesc = form.shortdesc.data
+    task = {'title':title,'shortdesc':shortdesc,'priority':priority}
 
     # set auto-generated key
-    r.set('T'+str(r.get('id')),'{"title":"'+str(title)+'","priority":"'+str(priority)+'"}')
+    r.hmset('T'+str(r.get('id')),task)
     r.incr('id')
     return redirect('/')
 
 def deleteTask(form):
     key = form.key.data
-    r.delete(key)
+    title = form.title.data
+    if(key):
+        r.delete(key)
+    else:
+        for i in r.keys():
+            if i!='id' and r.hget(i,'title')==title:
+                print i
+                r.delete(i)
     return redirect('/')
 
 def updateTask(form):
     title = form.title.data
     priority = form.priority.data
     key = form.key.data
+    shortdesc = form.shortdesc.data
+    task = {'title':title,'shortdesc':shortdesc,'priority':priority}
 
     # update by "reset"
-    r.set(key,'{"title":"'+str(title)+'","priority":"'+str(priority)+'"}')
+    if(r.exists(key)):
+        r.hmset(key,task)
     return redirect('/')
 
 def resetTask(form):
     r.flushall()
+    r.set('id','0')
     return redirect('/')
 
 @app.route('/', methods=['GET','POST'])
@@ -76,14 +96,10 @@ def main():
     val = {}
     for i in keys:
         if i!='id':
-            val[i]=json.loads(r[i])
+            val[i]=r.hgetall(i)
 
     return render_template('home.html', cform = cform, dform = dform, uform = uform, \
                 keys = keys, val = val, reset = reset)
 
 if __name__=='__main__':
-    # set auto-generated key
-    if(r.exists('id')==False):
-        r.set('id','0')
-
     app.run(debug=True)
